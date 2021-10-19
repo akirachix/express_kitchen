40edc584ed619e0a07bbef46b479654e50500e27
stella4545 <stellachepchumbah@gmail.com>
2021-10-19 13:21:51 +0300

    login

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Login',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +137,16 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL='/login'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'express_kitchen@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('Password')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 django_heroku.settings(locals())
