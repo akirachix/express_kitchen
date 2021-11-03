9d67b31bedd725f3b9632dcb8f2f117daf61cffb
adowmandek <adowmandek@gmail.com>
2021-11-03 08:25:18 +0300

    menu

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    name=models.CharField(max_length=10)
+    quantity=models.CharField(max_length=12)
+
+    def __str__(self):
+            return self.menu_nam
